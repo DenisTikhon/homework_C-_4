6c97e922d738aff90ac5b23b4f43ad82bb6481e4
DenisT <denis@tikhonov.pp.ru>
2022-08-23 12:19:58 +0300

    add task27

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿//Задача 25
-Console.WriteLine("число А");
-int A = Convert.ToInt32(Console.ReadLine());
+﻿int A = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("число B");
 int B = Convert.ToInt32(Console.ReadLine());
 int i = 1;
@@ -8,3 +6,12 @@ int st = A;
 while (i < B) {st = st * A; i++;} 
 Console.WriteLine(st);
 //Задача 27
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+string numbers = Convert.ToString(num);
+int i = 0;
+int sum = 0;
+while (numbers.Length > i) {sum += int.Parse(Convert.ToString(numbers[i])); i++;}
+Console.WriteLine(sum);
+//Задача 29
+
