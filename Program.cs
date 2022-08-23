c3d34e0fd1727089111b71b051f47e80332fb161
DenisT <denis@tikhonov.pp.ru>
2022-08-23 10:25:46 +0300

    add task25

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 25
+Console.WriteLine("число А");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("число B");
+int B = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int st = A;
+while (i < B) {st = st * A; i++;} 
+Console.WriteLine(st);
+//Задача 27
